c027f9848f1bbe1a6e3fe72a2c00d6fae6ee70f3
mattfookoo <matthewfukudome@gmail.com>
2023-04-08 13:28:36 -0700

    added 3d styling to the birthday card, added a .log message <3

---
+++ public/3dCard.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('.card-container');
+const card = document.querySelector('#birthday');
+
+card.addEventListener('mousemove', (e) => {
+  const xAxis = (card.getBoundingClientRect().left + (card.offsetWidth / 2) - e.pageX) / 25;
+  const yAxis = (card.getBoundingClientRect().top + (card.offsetHeight / 2) - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+// Reset the card position when the mouse leaves the container
+card.addEventListener('mouseleave', () => {
+  card.style.transform = 'rotateY(0deg) rotateX(0deg)';
+});
