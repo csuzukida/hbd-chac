9c8e105f0c48d196135bc9ed4464dbb51d96dae1
Christopher Suzukida <chris.suzukida@gmail.com>
2023-04-08 22:25:58 -0700

    Change styling and add video

--- public/birthday.js
+++ public/birthday.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js';
+import { storage } from 'https://www.gstatic.com/firebasejs/9.19.1/firebase-storage.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
